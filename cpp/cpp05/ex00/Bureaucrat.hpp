9d206e33ce0d72c9b58be8c449f3887be1634d7b
Lhousseine Chibani <lhchiban@e3r1p11.1337.ma>
2026-03-01 14:48:32 +0000

    recovery

--- cpp/cpp05/ex00/Bureaucrat.hpp
+++ cpp/cpp05/ex00/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: lhchiban <lhchiban@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/24 23:26:27 by lhchiban          #+#    #+#             */
-/*   Updated: 2026/01/29 12:58:16 by lhchiban         ###   ########.fr       */
+/*   Updated: 2026/03/01 13:39:33 by lhchiban         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,13 +23,7 @@ class Bureaucrat
     protected:
         const std::string name;
         int   grade;
-    public:
-        Bureaucrat();
-        Bureaucrat(int newGrade, const std::string& newName);
-        Bureaucrat(const Bureaucrat& Other);
-        Bureaucrat& operator=(const Bureaucrat& Other);
-        ~Bureaucrat();
-
+    public:                                
         const std::string& getName() const;
         int getGrade() const;
         void inc_burGrade(int inc_grade);

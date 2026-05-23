881dd975ad84bd8eac5938194f28080af77f1d32
Lhousseine Chibani <lhchiban@e3r7p16.1337.ma>
2026-05-23 18:58:05 +0100

    goo

--- cpp06/ex00/main.cpp
+++ cpp06/ex00/main.cpp
@@ -4,7 +4,7 @@
 
 int main(int ac, char **av)
 {
-    if (!av[1] || !av[1][0] || ac != 2)
+    if (!av[1] || ac != 2)
         return (std::cout << "Usage: " << av[0] << " <input_literal>" << "\n", 1);
     ScalarConverter::convert(av[1]);
     return 0;
